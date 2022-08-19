82c7675f6a1c8847f3d55c75f5f5adc5c2134cf0
Mehedi Bhuiyan <mehedibuiyan734@gmail.com>
2022-08-19 17:54:05 +0600

    fourth commit

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -1,8 +1,22 @@
 import styles from "../style";
 import { arrowUp } from "../assets";
+import {motion} from "framer-motion"
 
 const GetStarted = () => (
-  <div className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
+  <motion.div initial={{ opacity: 0, scale: 0.5 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{
+    default: {
+      duration: 0.3,
+      ease: [0, 0.71, 0.2, 1.01]
+    },
+    scale: {
+      type: "spring",
+      damping: 5,
+      stiffness: 100,
+      restDelta: 0.001
+    }
+  }} className={`${styles.flexCenter} w-[140px] h-[140px] rounded-full bg-blue-gradient p-[2px] cursor-pointer`}>
     <div className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full`}>
       <div className={`${styles.flexStart} flex-row`}>
         <p className="font-poppins font-medium text-[18px] leading-[23.4px]">
@@ -15,7 +29,7 @@ const GetStarted = () => (
         <span className="text-gradient">Started</span>
       </p>
     </div>
-  </div>
+  </motion.div>
 );
 
 export default GetStarted;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import styles from "../style";
 import { discount, robot } from "../assets";
 import GetStarted from "./GetStarted";
+import {motion} from "framer-motion"
 
 const Hero = () => {
   return (
@@ -34,15 +35,20 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
-        <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
+      <motion.div initial={{x:200, opacity:0.5, scale:0.5}} animate={{x:0, opacity:1, scale:1}}  transition={{ ease: "easeOut", duration: 1,scale: {
+      type: "spring",
+      damping: 5,
+      stiffness: 100,
+      restDelta: 0.001
+    } }} className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img  src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]" />
 
         {/* gradient start */}
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
         <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
         {/* gradient end */}
-      </div>
+      </motion.div>
 
       <div className={`ss:hidden ${styles.flexCenter}`}>
         <GetStarted />
